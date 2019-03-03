794f46b6423dfbb4c0eff35431504e1c7ea5d761
Bettyrosew <bettyrosaw@gmail.com>
2019-03-03 21:36:15 +0300

    edit new functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
+$(document).ready(function() {
+  $('button#submit').click(function() {
+    $(".login").hide();
+    $(".game").show();
+  });
+});
+//User Interface Logic
 function roll1() {
   var r1=Math.floor(Math.random() * 6) +1;
   var diceScore=r1;
   if (diceScore!=1) {
-    total1+=diceScore
+    score1+=diceScore
   }else  {
-    total1=0;
+    score1=0;
   }
   result1.innerHTML=("You have rolled " + r1)
-  points1.innerHTML=("Your total points are " + total1)
+  points1.innerHTML=("Your total points are " + score1)
 }
 function roll2() {
   var r2=Math.floor(Math.random() * 6) +1;
   var diceScore=r2;
   if (diceScore!=1) {
-    total2+=diceScore
+    score2+=diceScore
   }else {
-    total2=0;
+    score2=0;
   }
   result2.innerHTML=("You have rolled " + r2)
-  points2.innerHTML=("Your total points are " + total2)
+  points2.innerHTML=("Your total points are " + score2)
+}
+function hold1() {
+  total1+=score1
+ alert("You have " +total1 +" points")
+ if (total1==100){
+   alert("You have won the game!Hurray!")
+ }else {
+   roll2()
+ }
+
 }
