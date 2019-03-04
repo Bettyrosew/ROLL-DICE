90df2a41221acc8f5fbb0c9def158e9b5b44b280
Bettyrosew <bettyrosaw@gmail.com>
2019-03-04 13:04:17 +0300

    edit new jquery functions in the file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
-$(document).ready(function() {
-  $('button#submit').click(function() {
-    $(".login").hide();
-    $(".game").show();
-  });
-});
+// Business Interface Logic
+function Player (first,last) {
+  this.firstName=first;
+  this.secondName=last;
+}
+Player.prototype.fullName=function() {
+  return this.firstName+ " " + this.secondName;
+}
 //User Interface Logic
 function roll1() {
-  var r1=Math.floor(Math.random() * 6) +1;
-  var diceScore=r1;
-  if (diceScore!=1) {
-    score1+=diceScore
+  var diceScore=Math.floor(Math.random() * 6) +1;
+  if (diceScore!==1) {
+    score1+=diceScore;
   }else  {
     score1=0;
   }
-  result1.innerHTML=("You have rolled " + r1)
-  points1.innerHTML=("Your total points are " + score1)
+  result1.innerHTML=("You have rolled " + diceScore);
+  points1.innerHTML=(score1);
 }
 function roll2() {
-  var r2=Math.floor(Math.random() * 6) +1;
-  var diceScore=r2;
+  var diceScore=Math.floor(Math.random() * 6) +1;
   if (diceScore!=1) {
     score2+=diceScore
   }else {
     score2=0;
   }
-  result2.innerHTML=("You have rolled " + r2)
-  points2.innerHTML=("Your total points are " + score2)
+  result2.innerHTML=("You have rolled " + diceScore)
+  points2.innerHTML=(score2)
 }
 function hold1() {
   total1+=score1
@@ -37,3 +37,35 @@ function hold1() {
  }
 
 }
+
+$(document).ready(function() {
+  $("#roll1").click(function() {
+    roll1();
+  });
+  $("#roll2").click(function() {
+    roll2();
+  })
+  $("form#new-player").submit(function(event) {
+    event.preventDefault();
+
+    $(".login").hide();
+    $(".game").show();
+
+    var inputtedFirstName1 = $("input#new-first-name1").val();
+    var inputtedSecondName1 = $("input#new-second-name1").val();
+    var inputtedFirstName2=$("input#new-first-name2").val();
+    var inputtedSecondName2=$("input#new-second-name2").val();
+
+    var newPlayer1 = new Player(inputtedFirstName1, inputtedSecondName1);
+    var newPlayer2=new Player(inputtedFirstName2, inputtedSecondName2);
+
+    $("input#new-first-name1").val("");
+    $("input#new-second-name1").val("");
+    $("input#new-first-name2").val("");
+    $("input#new-second-name2").val("");
+      $(".first-name1").text(newPlayer1.firstName);
+      $(".second-name1").text(newPlayer1.secondName);
+      $(".first-name2").text(newPlayer2.firstName);
+      $(".second-name2").text(newPlayer2.secondName);
+  });
+});
